86522c3999e1723b07d70ebb8a3d7df8d1f10250
Kaveen chandupa <123149615+AKCadhikari@users.noreply.github.com>
2024-06-24 17:11:44 +0530

    Add files via upload

---
+++ J2.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.w3;
+
+
+public class J2 {
+   public static void main(String[]args){
+       int maxmarks=500;
+       int usermarks=423;
+       
+       float percentage= (float)usermarks/maxmarks*100.0f;
+       
+       System.out.println("percentage is"+percentage);
+       
+   }
+}
---
+++ j1.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.w3;
+
+
+public class j1 {
+    public static void main(String[]args){
+            double mydouble=9.9;
+            int myint= (int)mydouble;
+            
+    System.out.println(mydouble);
+    System.out.println(myint);
+}
+}
