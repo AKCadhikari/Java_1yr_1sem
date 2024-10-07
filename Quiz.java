a959889717c73da7c82273930fb65d378380baf1
Kaveen chandupa <aakchandupa@gmail.com>
2024-10-07 20:07:28 +0530

    java sampe quiz

---
+++ Quiz.java
@@ -0,0 +1,13 @@
+public class Quiz{
+    public static float Farenhite(float c){
+        float f = c+52;
+        System.out.println("temperature is"+f+"farenheite");
+        return f;
+        
+    }
+    public static void main(String[] args){
+        Farenhite(77);
+        String date="Monday";
+        System.out.println(date);
+    }
+}
